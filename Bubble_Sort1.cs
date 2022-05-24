d925256f0f7614dc009486a827ab49ed96893317
Burak Furkan ERKEMEN <84676805+BFEisHERE@users.noreply.github.com>
2022-05-24 18:24:16 +0300

    Add files via upload

---
+++ Bubble_Sort1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Bubble_Sort2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sıralanacak dizi oluşturuyoruz
+            int[] diz = {25,57,48,37,12,92,86,33};
+            int tut;
+            //Dizi indislerini gezilecek
+            for (int j = 0; j <= diz.Length-2; j++)
+            {
+                for (int i = 0; i <= diz.Length-2; i++)
+                {
+                    if (diz[i] > diz[i+1]) 
+                    {
+                        tut = diz[i+1];
+                        diz[i+1] = diz[i];
+                        diz[i] = tut;
+                    }
+                }
+            }
+            Console.Write("Sıralama: ");
+            foreach (int a in diz)
+            {
+                Console.Write(a + " ");
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ Bubble_Sort3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Bubble_Sort3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = { 9, 5, 8, 3, 1 };
+            int tut;
+            for (int j = 0; j <= dizi.Length-2; j++)
+            {
+                for (int i = 0; i <= dizi.Length-2; i++)
+                {
+                    if (dizi[i] > dizi[i+1])
+                    {
+                        //Küçükse tutuyoruz
+                        tut = dizi[i + 1];
+                        //üst indisle büyük alt indisi yer değiştiriyorz
+                        dizi[i + 1] = dizi[i];
+                        //üst indisteki küçük değeri alt indise yerleştiriyoruz
+                        dizi[i] = tut;
+                    }
+                }
+            }
+            Console.Write("Listenin Sıralaması: ");
+            foreach(int a in dizi)
+                Console.Write(a + " ");
+            Console.ReadKey();
+        }
+    }
+}
