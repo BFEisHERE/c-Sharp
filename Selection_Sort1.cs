8ced49d80456d1cc5f3048f88e7a20a6ae514772
Burak Furkan ERKEMEN <84676805+BFEisHERE@users.noreply.github.com>
2022-05-25 05:05:15 +0300

    Add files via upload

---
+++ Selection_Sort1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Selection_Sort1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //dizi tanımlama
+            int[] dizi = new int[10] { 56, 1, 99, 67, 89, 23, 44, 12, 78, 34 };
+            int n = 10; // dizi boyutu
+            Console.WriteLine("Selection Sort");
+            Console.Write("İlk Dizi:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(dizi[i]+" ");
+            }
+            int tut, enkucuk;
+            for (int i = 0; i < n-1; i++)
+            {
+                //sözde en küçük değer bulundu karşılaştırma için
+                enkucuk = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (dizi[j] < dizi[enkucuk])
+                    {
+                        //en küçüğü bulmak için for döngüsünde döndürüldü
+                        enkucuk = j; 
+                    }
+                }
+                //yer değiştirme yaptık
+                tut = dizi[enkucuk];
+                dizi[enkucuk] = dizi[i];
+                dizi[i] = tut;
+            }
+            Console.WriteLine();
+            Console.Write("Sıralanmış Hali: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(dizi[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Selection_Sort2.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Selection_Sort2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // dizi tanımlama ve adedini belirleme
+            int[] dizi = new int[5] { 9, 5, 8, 3, 1 };
+            int n = 5;
+            //ilk diziyi yazdıralım
+            Console.WriteLine("Selection Sort");
+            Console.Write("İlk Dizi: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(dizi[i] + " ");
+            }
+            //karşılaştırıp sıralama
+            int tut, enkucuk;
+            for (int i = 0; i < n-1; i++)
+            {
+                enkucuk = i;
+
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (dizi[j] < dizi[i])
+                    {
+                        enkucuk = j;
+                    }
+                }
+                /* j indisindeki değer i indisindekinden küçüktür
+                 Bu yüzden tut değerine dizinin en küçük değeri olan j indisindeki değer tutturuldu
+                normal indisteki değerle eşitlendi 
+                ve i indisindeki küçük değer yazıldı
+                 */ 
+                
+                tut = dizi[enkucuk];
+                dizi[enkucuk] = dizi[i];
+                dizi[i] = tut;
+            }
+            Console.WriteLine();
+            Console.Write("Sıralı Liste:");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(dizi[i]+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
